ee19d39b21c62b476ebffea52e8a77db467cc9fb
RJ2506 <77298403+RJ2506@users.noreply.github.com>
2021-03-08 12:20:11 -0800

    added js

---
+++ app.js
@@ -0,0 +1,14 @@
+let num1 = 25
+let num2 = 16
+const names = 'Rodolf'
+const booleans = false
+
+if (typeof num1 === typeof num2){
+    console.log("they match")
+}
+else 
+    console.log("no luck")
+
+for (char of names){
+    console.log(char)
+}
